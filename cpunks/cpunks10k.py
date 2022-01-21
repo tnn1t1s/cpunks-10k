dddd91ff9a5cc0d3f9469a93583bbdd4600a79a3
Jms Dnns <jdennis@gmail.com>
2022-01-20 22:35:10 -0500

    Formatted everything according to flake8

--- cpunks/cpunks10k.py
+++ cpunks/cpunks10k.py
@@ -1,27 +1,28 @@
 import numpy as np
 import pandas as pd
 
+
 class cpunks10k:
     def __init__(self):
         self.punks_df = pd.read_pickle('../data/punks.pkl')
         self.labels = list(self.punks_df.dtypes[0:-1].index)
-        
+
     def any_to_one(self, i):
-        x= True
+        x = True
         if i['any']:
-            x=False
+            x = False
         return int(x)
 
     def load_data(self, labels=None):
-        if labels == None:
-            labels=self.labels
+        if labels is None:
+            labels = self.labels
         df = self.punks_df.copy()[self.labels + ['img']]
         df['any'] = df[self.labels].apply(np.any, axis=1)
         df['none'] = df.apply(lambda x: self.any_to_one(x), axis=1)
         df = df.drop(['any'], axis=1)
         X = df['img'].to_numpy()
         Y = df[self.labels + ['none']].to_numpy()
-        X  = np.array([row[0] for row in X])
+        X = np.array([row[0] for row in X])
         X_train = X[0:9000]
         Y_train = Y[0:9000]
         X_test = X[9000:10000]
--- models/AE.py
+++ models/AE.py
@@ -1,31 +1,23 @@
-from tensorflow.keras.layers import Input, Conv2D, Flatten, Dense, Conv2DTranspose, Reshape, Lambda, Activation, BatchNormalization, LeakyReLU, Dropout
-from tensorflow.keras.models import Model
-from tensorflow.keras import backend as K
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint 
-from tensorflow.keras.utils import plot_model
+from tensorflow.python.keras.layers import (
+    Input, Conv2D, Flatten, Dense, Conv2DTranspose, Reshape,
+    Activation, BatchNormalization, LeakyReLU, Dropout
+)
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.optimizers import Adam
 
-from utils.callbacks import CustomCallback, step_decay_schedule
+from utils.callbacks import step_decay_schedule
 
 import numpy as np
-import json
-import os
-import pickle
 
 
 class Autoencoder():
-    def __init__(self
-        , input_dim
-        , encoder_conv_filters
-        , encoder_conv_kernel_size
-        , encoder_conv_strides
-        , decoder_conv_t_filters
-        , decoder_conv_t_kernel_size
-        , decoder_conv_t_strides
-        , z_dim
-        , use_batch_norm = False
-        , use_dropout = False
-        ):
+    def __init__(
+        self, input_dim, encoder_conv_filters, encoder_conv_kernel_size,
+        encoder_conv_strides, decoder_conv_t_filters,
+        decoder_conv_t_kernel_size, decoder_conv_t_strides, z_dim,
+        use_batch_norm=False, use_dropout=False
+    ):
 
         self.name = 'autoencoder'
 
@@ -48,19 +40,19 @@ class Autoencoder():
 
     def _build(self):
 
-        ### THE ENCODER
+        # THE ENCODER #####
         encoder_input = Input(shape=self.input_dim, name='encoder_input')
 
         x = encoder_input
 
         for i in range(self.n_layers_encoder):
             conv_layer = Conv2D(
-                filters = self.encoder_conv_filters[i]
-                , kernel_size = self.encoder_conv_kernel_size[i]
-                , strides = self.encoder_conv_strides[i]
-                , padding = 'same'
-                , name = 'encoder_conv_' + str(i)
-                )
+                filters=self.encoder_conv_filters[i],
+                kernel_size=self.encoder_conv_kernel_size[i],
+                strides=self.encoder_conv_strides[i],
+                padding='same',
+                name='encoder_conv_' + str(i)
+            )
 
             x = conv_layer(x)
 
@@ -70,17 +62,16 @@ class Autoencoder():
                 x = BatchNormalization()(x)
 
             if self.use_dropout:
-                x = Dropout(rate = 0.25)(x)
+                x = Dropout(rate=0.25)(x)
 
         shape_before_flattening = K.int_shape(x)[1:]
 
         x = Flatten()(x)
-        encoder_output= Dense(self.z_dim, name='encoder_output')(x)
+        encoder_output = Dense(self.z_dim, name='encoder_output')(x)
 
         self.encoder = Model(encoder_input, encoder_output)
 
-
-        ### THE DECODER
+        # THE DECODER #####
         decoder_input = Input(shape=(self.z_dim,), name='decoder_input')
 
         x = Dense(np.prod(shape_before_flattening))(decoder_input)
@@ -88,23 +79,23 @@ class Autoencoder():
 
         for i in range(self.n_layers_decoder):
             conv_t_layer = Conv2DTranspose(
-                filters = self.decoder_conv_t_filters[i]
-                , kernel_size = self.decoder_conv_t_kernel_size[i]
-                , strides = self.decoder_conv_t_strides[i]
-                , padding = 'same'
-                , name = 'decoder_conv_t_' + str(i)
-                )
+                filters=self.decoder_conv_t_filters[i],
+                kernel_size=self.decoder_conv_t_kernel_size[i],
+                strides=self.decoder_conv_t_strides[i],
+                padding='same',
+                name='decoder_conv_t_' + str(i)
+            )
 
             x = conv_t_layer(x)
 
             if i < self.n_layers_decoder - 1:
                 x = LeakyReLU()(x)
-                
+
                 if self.use_batch_norm:
                     x = BatchNormalization()(x)
-                
+
                 if self.use_dropout:
-                    x = Dropout(rate = 0.25)(x)
+                    x = Dropout(rate=0.25)(x)
             else:
                 x = Activation('sigmoid')(x)
 
@@ -112,12 +103,11 @@ class Autoencoder():
 
         self.decoder = Model(decoder_input, decoder_output)
 
-        ### THE FULL AUTOENCODER
+        # THE FULL AUTOENCODER #####
         model_input = encoder_input
         model_output = self.decoder(encoder_output)
 
         self.model = Model(model_input, model_output)
-
 
     def compile(self, learning_rate):
         self.learning_rate = learning_rate
@@ -125,23 +115,23 @@ class Autoencoder():
         optimizer = Adam(lr=learning_rate)
 
         def r_loss(y_true, y_pred):
-            return K.mean(K.square(y_true - y_pred), axis = [1,2,3])
+            return K.mean(K.square(y_true - y_pred), axis=[1, 2, 3])
 
-        self.model.compile(optimizer=optimizer, loss = r_loss)
+        self.model.compile(optimizer=optimizer, loss=r_loss)
 
-    def train(self, x_train, batch_size, epochs, print_every_n_batches = 100, initial_epoch = 0, lr_decay = 1):
+    def train(
+        self, x_train, batch_size, epochs, print_every_n_batches=100,
+        initial_epoch=0, lr_decay=1
+    ):
 
-        lr_sched = step_decay_schedule(initial_lr=self.learning_rate, decay_factor=lr_decay, step_size=1)
-
+        lr_sched = step_decay_schedule(
+            initial_lr=self.learning_rate, decay_factor=lr_decay, step_size=1
+        )
 
         callbacks_list = [lr_sched]
 
-        self.model.fit(     
-            x_train
-            , x_train
-            , batch_size = batch_size
-            , shuffle = True
-            , epochs = epochs
-            , initial_epoch = initial_epoch
-            , callbacks = callbacks_list
+        self.model.fit(
+            x_train, x_train, batch_size=batch_size, shuffle=True,
+            epochs=epochs, initial_epoch=initial_epoch,
+            callbacks=callbacks_list
         )
