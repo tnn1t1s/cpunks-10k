e261b652d6649403221716c4a57f746aaeed4141
tnn1t1s <david.j.palaitis@gmail.com>
2022-01-21 09:50:44 -0500

    load file relative to root and clean up labels

--- cpunks/cpunks10k.py
+++ cpunks/cpunks10k.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
+import pathlib
 
 
 class cpunks10k:
     def __init__(self):
-        self.punks_df = pd.read_pickle('../data/punks.pkl')
-        self.labels = list(self.punks_df.dtypes[0:-1].index)
+        module_root = pathlib.Path(__file__).parents[1]
+        self.punks_df = pd.read_pickle(f"{module_root}/data/punks.pkl")
 
     def any_to_one(self, i):
         x = True
@@ -14,18 +15,23 @@ class cpunks10k:
         return int(x)
 
     def load_data(self, labels=None):
+        '''todo: reasonably handle labels or punt to another function,
+                 flexible insample/outsample scheme,
+                 think through insample/outsample to mix and insure
+                 reasonable distribution of types.
+        '''
         if labels is None:
-            labels = self.labels
-        df = self.punks_df.copy()[self.labels + ['img']]
-        df['any'] = df[self.labels].apply(np.any, axis=1)
+            labels = list(self.punks_df.dtypes[0:-1].index)
+        df = self.punks_df.copy()[labels + ['img']]
+        df['any'] = df[labels].apply(np.any, axis=1)
         df['none'] = df.apply(lambda x: self.any_to_one(x), axis=1)
+
         df = df.drop(['any'], axis=1)
         X = df['img'].to_numpy()
-        Y = df[self.labels + ['none']].to_numpy()
+        Y = df[labels + ['none']].to_numpy()
         X = np.array([row[0] for row in X])
         X_train = X[0:9000]
         Y_train = Y[0:9000]
         X_test = X[9000:10000]
         Y_test = Y[9000:10000]
-        labels = self.labels + ['none']
         return (X_train, Y_train), (X_test, Y_test), (labels)
